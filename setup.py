eef375bc09832ed1f794c70550e6ab331cab284b
Naren <narendas10@gmail.com>
2024-01-08 07:37:57 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 import os
 
+with open("README.md", "r", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
 
 def get_requirements(file_path):
     requirements = []
@@ -14,16 +17,18 @@ def get_requirements(file_path):
 
 PROJECT_NAME = "naren_api"
 VERSION = "0.0.1"
-DESCRIPTION = "<ENTER_DESCRIPTION_HERE>"
+DESCRIPTION = "A small python package"
 AUTHOR = "NarenBot"
 AUTHOR_EMAIL = "narendas10@gmail.com"
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
-    description=DESCRIPTION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content="text/markdown",
     package_dir={"": "src"},  # "src"
     packages=find_packages(where="src"),  # "where='src'"
     # install_requires=get_requirements("requirements.txt"),
